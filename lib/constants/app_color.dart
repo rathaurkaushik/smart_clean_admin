cf77daff6d3bc5bb785b0d5b1864ad81a9cf6917
kaushik <rathaurkaushik015@gmail.com>
2025-03-08 19:05:01 +0530

    add color

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -8,7 +8,7 @@ class AppColor{
   static const grayTextColor = Color(0xff6b7280);
   static const numberHeighlightedColor = Color(0xff2563eb);
   static const warningColor = Color(0xffea580c);
-  static const blackColor = Color(0xff000000);
+  static const appBlackColor = Color(0xff000000);
   static const simpleTextColor = Color(0xff1f2937);
   static const pendingTextColor = Color(0xffa16207);
   static const pendingBackgroundColor = Color(0xfffef9c3);
